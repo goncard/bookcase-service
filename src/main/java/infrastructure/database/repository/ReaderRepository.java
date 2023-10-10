95a6328e938f7183f416ef56f490000ca1ca0d3c
goncalo.cardeira <goncalo.cardeira@gmail.com>
2023-10-10 17:30:36 +0100

    Setup database

---
+++ src/main/java/infrastructure/database/repository/ReaderRepository.java
@@ -0,0 +1,8 @@
+package infrastructure.database.repository;
+
+
+import infrastructure.database.entity.Reader;
+import io.quarkus.hibernate.orm.panache.PanacheRepository;
+
+public class ReaderRepository implements PanacheRepository<Reader> {
+}
